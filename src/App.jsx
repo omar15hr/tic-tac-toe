e08c7f371e92ef89bd1ce7850fe94fac8243d0d6
omar <omarhrn@gmail.com>
2025-02-08 12:13:37 -0300

    winner draw

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 const TURNS = { X: "x", O: "o" };
 
 const Square = ({ children, isSelected, updateBoard, index }) => {
-
   const className = `square ${isSelected ? "is-selected" : ""}`;
 
   const handleClick = () => {
@@ -17,12 +16,38 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   );
 };
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    return null;
+  }
 
   const updateBoard = (index) => {
-    if (board[index]) return;
+    if (board[index] || winner) return;
 
     const newBoard = [...board];
     newBoard[index] = turn;
@@ -30,11 +55,29 @@ function App() {
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false);
+    }
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  }
+
+  const checkEndGame = (newBoard) => {
+    return newBoard.every(square => square !== null);
   }
 
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reset the game</button>
 
       <section className="game">
         {board.map((_, index) => {
@@ -54,6 +97,30 @@ function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+
+      {
+        winner !== null && (
+          <section className="winner">
+            <div className="text">
+              <h2>
+                {
+                  winner === false
+                    ? "It's a draw!"
+                    : `The winner is`
+                }
+              </h2>
+
+              <header className="win">
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                <button onClick={resetGame}>Play Again</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
 
     </main>
   );
